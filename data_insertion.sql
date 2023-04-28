7a63e74dcee1441465bb639df88fbbecac12b20e
Lux <jlaxman@ec.iitr.ac.in>
2023-04-27 20:30:41 -0400

    Added data insertion sql script

---
+++ data_insertion.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE  IF NOT EXISTS `OnlineAuctionSystem`;
+USE `OnlineAuctionSystem`;
+
+INSERT INTO `userlogin` VALUES ('Vennela','chava.vinni@gmail.com','vinni','candy',NULL,NULL,'Laptop'),('Gayathr1','gr485@gmail.com','gaya3','heythere',NULL,NULL,'Mobile'),('Rohit','roh123@gmail.com','roh', 'abcda','415-567-1289','1500 Market St.','Accessory'),('Laxman','sanju@gmail.com','sanju', 'hello','345-567-1289',NULL,'Mobile');
