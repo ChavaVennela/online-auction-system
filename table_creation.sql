9fd3976ca5ea72bb6c9f28e049066c31e74a9057
Lux <jlaxman@ec.iitr.ac.in>
2023-04-27 23:21:28 -0400

    updated data_insertion, images in product folder

--- table_creation.sql
+++ table_creation.sql
@@ -12,8 +12,6 @@ CREATE TABLE `userlogin` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `userlogin` VALUES ('Vennela','chava.vinni@gmail.com','vinni','candy',NULL,NULL,'Laptop'),('Gayathr1','gr485@gmail.com','gaya3','heythere',NULL,NULL,'Mobile'),('Rohit','roh123@gmail.com','roh', 'abcda','415-567-1289','1500 Market St.','Accessory'),('Laxman','sanju@gmail.com','sanju', 'hello','345-567-1289',NULL,'Mobile');
-
 DROP TABLE IF EXISTS `CustomerReps`;
 CREATE TABLE `CustomerReps` (
   `username` varchar(30) NOT NULL,
